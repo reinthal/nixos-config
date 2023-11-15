59a06dc41da44897be3d6a5a8000a7935ed2f32c
kog <kog@relay.com>
2023-11-16 00:36:27 +0100

    add tor

--- modules/tor.nix
+++ modules/tor.nix
@@ -1,8 +1,14 @@
 { config, pkgs, ... }:
 {
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-        tor
-    ];
+    services.tor.enable = true; 
+    environment.etc."tor/torrc".text = pkgs.lib.mkForce ''
+Nickname      HotTacoRelay 
+ContactInfo   hot.taco.relay@protonmail.com
+ORPort        443
+ExitRelay     0
+SocksPort     0
+Log notice    syslog
+DataDirectory /var/lib/tor
+User          tor
+''; 
 }
