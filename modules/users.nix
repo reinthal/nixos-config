4f70b03919fe389cfbba28531b425029a87d0fa8
alex <arch@battlestation.me>
2023-11-15 11:23:45 +0100

    add user

---
+++ modules/users.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    # Set this explicitly to where e.g. zprezto expects it
+    histFile = "$HOME/.config/zsh/.zhistory";
+  };
+
+  security.sudo.enable = true;
+  security.sudo.wheelNeedsPassword = true; 
+
+  users.users.kog = {
+    description = "kog";
+    isNormalUser = true;
+    extraGroups = [ 
+		 "networkmanager" 
+		 "wheel"
+		 "docker"
+    ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+		git
+		zsh
+		nmap
+		lf    
+    ];
+    uid = 1000;
+    openssh.authorizedKeys.keyFiles = [ 
+        ../keys/battlestation.pub
+        ../keys/mbpro.pub 
+    ];
+  };
+}
