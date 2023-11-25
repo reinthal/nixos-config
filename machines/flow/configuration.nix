a8d607c8e1ffcc40e3f015aa7022e36f9a118baf
reinthal <reinthal@flow.local>
2023-11-25 20:33:37 +0100

    add autoupgrade

--- machines/flow/configuration.nix
+++ machines/flow/configuration.nix
@@ -41,6 +41,10 @@
 	# this value at the release version of the first install of this system.
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "23.05"; # Did you read the comment?
+	system = {
+             autoUpgrade.enable = true;
+             autoUpgrade.allowReboot = true;
 
+            stateVersion = "23.05"; # Did you read the comment?
+        };
 }
