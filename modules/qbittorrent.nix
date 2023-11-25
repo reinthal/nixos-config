b80e03457ff4fbcd815ab304ff11058b1cd61b4f
reinthal <reinthal@flow.local>
2023-11-25 22:34:46 +0100

    reconfigure volume

--- modules/qbittorrent.nix
+++ modules/qbittorrent.nix
@@ -4,8 +4,14 @@
      qbittorrent = {
        image = "lscr.io/linuxserver/qbittorrent:latest";
        ports = ["8080:8080" "5225:5225"];
+       environment = {
+          PUID = "1000";
+          PGID = "100";
+          UMASK = "002";
+          TZ="Etc/Stockholm";
+       };
        volumes = [
-         "/home/kog/dockers/flow/qbittorrent/appdata/:/config"
+         "/home/kog/appdata/qbittorrent:/config"
          "/mnt/media/torrent:/data/torrent"
        ];
      };
