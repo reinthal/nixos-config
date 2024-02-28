141f8ba81deb468748efe031ad55b423054aac5e
reinthal <reinthal@flow.local>
2024-02-28 22:52:55 +0100

    reconfigure nas to skff

--- modules/nas-client.nix
+++ modules/nas-client.nix
@@ -5,13 +5,13 @@
 
     environment.systemPackages = with pkgs; [cifs-utils];
     # Filesystem related settings
-    fileSystems."/mnt/media" = {
-        device = "//nas.reinthal.me/media";
+    fileSystems."/mnt/skff" = {
+        device = "//nas.reinthal.me/skff";
         fsType = "cifs";
         options = let
     # this line prevents hanging on network split
     automount_opts = "_netdev,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    permissions = "nobrl,dir_mode=0755,file_mode=0664,uid=1000,gid=100,noperm";
+    permissions = "nobrl,dir_mode=0755,file_mode=0664,uid=1337,gid=100,noperm";
         in ["${permissions},${automount_opts},credentials=/etc/smb-secrets"];
     };
 }
