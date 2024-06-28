c8e528d470243d392e63bcb7e569c32f8892afda
Alexander Reinthal <reinthal@pm.me>
2024-06-28 14:15:50 +0200

    more dead code

--- hosts/build/configuration.nix
+++ hosts/build/configuration.nix
@@ -105,18 +105,7 @@
     gnome.gnome-remote-desktop
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 3389 ];
