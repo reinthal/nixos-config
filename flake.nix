f290e32b65e2e7ee79580444fbb0d3845b3992a7
Alexander Reinthal <reinthal@pm.me>
2024-06-28 14:43:59 +0200

    add build server

--- flake.nix
+++ flake.nix
@@ -11,12 +11,21 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-    };
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+          ./hosts/default/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      build = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+          ./hosts/build/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
   };
 }
