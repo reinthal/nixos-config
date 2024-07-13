64b80bfd53d5625d2a51274179475f3ffb3c80c8
Alexander Reinthal <email@reinthal.me>
2024-07-13 21:21:26 +0200

    wip: remove gunk

--- flake.nix
+++ flake.nix
@@ -3,37 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-
-    
-    hyprland-hyprspace = {
-      url = "github:KZDKM/Hyprspace";
-      inputs.hyprland.follows = "hyprland";
-    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    matugen.url = "github:InioX/matugen?ref=v2.2.0";
-      ags.url = "github:Aylur/ags";
-      astal.url = "github:Aylur/astal";
-
-      lf-icons = {
-        url = "github:gokcehan/lf";
-        flake = false;
-      };
-
-      firefox-gnome-theme = {
-        url = "github:rafaelmardojai/firefox-gnome-theme";
-        flake = false;
-      };
 
   };
 
@@ -41,14 +15,6 @@
 
   outputs = { self, nixpkgs, ... } @inputs: {
     nixosConfigurations = {
-      
-      default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [
-          ./hosts/default/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
 
       build = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
