42836340ad9e443fb208797e086769eb59ad7833
Alexander Reinthal <email@reinthal.me>
2024-07-13 21:30:26 +0200

    add mbp, remove build from flake

--- flake.nix
+++ flake.nix
@@ -2,27 +2,49 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Where we get most of our software. Giant mono repo with recipes
+    # called derivations that say how to build software.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-22.11
+    # Manages configs links things into your home directory
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Controls system level software and settings including fonts
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Tricked out nvim :)
+    pwnvim.url = "github:zmre/pwnvim";
 
   };
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    darwin,
+    pwnvim,
+    ...
+  }: {
 
-   
-
-  outputs = { self, nixpkgs, ... } @inputs: {
-    nixosConfigurations = {
-
-      build = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [
-          ./hosts/build/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
+    darwinConfigurations.mbp = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        system = "aarch64-darwin";
+        config.allowUnfree = true;
       };
+      modules = [
+        ./modules/darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {inherit pwnvim;};
+            users.kog.imports = [
+              ./modules/home-manager
+            ];
+          };
+        }
+      ];
     };
   };
 }
