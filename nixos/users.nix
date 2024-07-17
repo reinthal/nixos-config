b0d2cbce487fdabfaa414d6eb241196ce048eb62
alex <email@reinthal.me>
2024-07-17 21:05:59 +0200

    refactor userrs

--- nixos/users.nix
+++ nixos/users.nix
@@ -12,7 +12,6 @@
     extraGroups = [ 
 		 "networkmanager" 
 		 "wheel"
-		 "docker"
     ];
     shell = pkgs.zsh;
     uid = 1337;
@@ -24,18 +23,15 @@
 
   users.users.kog = {
     description = "kog";
+    home = "/home/kog";
     isNormalUser = true;
     hashedPassword = "$6$WMQGBij0sndI.ZxH$eBSuf/DxQvBnPqGb0qlxXQsUPFkFWc3QKufpTSpfvKMXYcI/NkIZ51vE9vE36388Yj7QnmjACHu3Kbms6dFNm.";
     extraGroups = [ 
 		 "networkmanager" 
 		 "wheel"
-		 "docker"
     ];
     shell = pkgs.zsh;
     uid = 1000;
-    # To use this shell.nix on NixOS your user needs to be configured as such:
-    subUidRanges = [{ startUid = 100000; count = 65536; }];
-    subGidRanges = [{ startGid = 100000; count = 65536; }];
     openssh.authorizedKeys.keyFiles = [ 
         ./keys/battlestation.pub
         ./keys/mbpro.pub
