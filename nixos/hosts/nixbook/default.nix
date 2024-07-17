cc76f7f7ee0b88d6a0acaa7683b9ee851f561722
alex <email@reinthal.me>
2024-07-17 15:27:03 +0200

    configure default shell

--- nixos/hosts/nixbook/default.nix
+++ nixos/hosts/nixbook/default.nix
@@ -1,36 +1,40 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, pkgs, lib, inputs, outputs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      
-      # apple-silicon hardware support
-      inputs.apple-silicon.nixosModules.apple-silicon-support
-      
-      # enable various features
-      ../../features/sound.nix
-      ../../features/bluetooth.nix
+  config,
+  pkgs,
+  lib,
+  inputs,
+  outputs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
 
-      # font config
-      ../../features/hidpi.nix
+    # apple-silicon hardware support
+    inputs.apple-silicon.nixosModules.apple-silicon-support
 
-      # Import home-manager's NixOS module
-      inputs.home-manager.nixosModules.home-manager
-    ];
+    # enable various features
+    ../../features/sound.nix
+    ../../features/bluetooth.nix
+
+    # font config
+    ../../features/hidpi.nix
+
+    # Import home-manager's NixOS module
+    inputs.home-manager.nixosModules.home-manager
+  ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
   # Pick only one of the below networking options.
   networking.hostName = "nixbook"; # Define your hostname.
   networking.networkmanager = {
-    enable = true;  # Easiest to use and most distros use this by default.
+    enable = true; # Easiest to use and most distros use this by default.
     wifi.backend = "iwd";
-  }; 
+  };
   # enable GPU support and audio
   hardware.asahi = {
     useExperimentalGPUDriver = true;
@@ -39,55 +43,64 @@
   };
 
   # backlight control
-  programs.light.enable = true;  
+  programs.light.enable = true;
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      {
+        keys = [225];
+        events = ["key"];
+        command = "/run/current-system/sw/bin/light -A 10";
+      }
+      {
+        keys = [224];
+        events = ["key"];
+        command = "/run/current-system/sw/bin/light -U 10";
+      }
     ];
   };
- 
+
   networking.wireless.iwd = {
     enable = true;
-    
+
     settings.General.EnableNetworkConfiguration = true;
   };
   time.timeZone = "Europe/Stockholm";
 
   services.xserver = {
-  
     enable = true;
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
   };
   home-manager = {
-    extraSpecialArgs = { inherit inputs outputs; };
+    extraSpecialArgs = {inherit inputs outputs;};
     users = {
       kog = import ../../../home-manager;
     };
   };
+  programs.zsh.enable = true;
   users.users.kog = {
-     home = "/home/kog";  
-     isNormalUser = true;
-     hashedPassword = "$6$WMQGBij0sndI.ZxH$eBSuf/DxQvBnPqGb0qlxXQsUPFkFWc3QKufpTSpfvKMXYcI/NkIZ51vE9vE36388Yj7QnmjACHu3Kbms6dFNm.";
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       firefox
-       tree
-       git
-     ];
-   };
+    home = "/home/kog";
+    isNormalUser = true;
+    hashedPassword = "$6$WMQGBij0sndI.ZxH$eBSuf/DxQvBnPqGb0qlxXQsUPFkFWc3QKufpTSpfvKMXYcI/NkIZ51vE9vE36388Yj7QnmjACHu3Kbms6dFNm.";
+    shell = pkgs.zsh;
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      firefox
+      tree
+      git
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     (vscode-with-extensions.override {
-    vscode = vscodium;
-    vscodeExtensions = with vscode-extensions; [
-      bbenoist.nix
-    ]; 
-  })
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+      ];
+    })
   ];
   # Most users should NEVER change this value after the initial install, for any reason,
   # even if you've upgraded your system to a new NixOS release.
@@ -104,6 +117,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
-
