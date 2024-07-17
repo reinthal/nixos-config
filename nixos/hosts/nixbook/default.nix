33cb8892950bb045abe2cf13990bedf89d2d3768
alex <email@reinthal.me>
2024-07-17 07:45:42 +0200

    add nixbook config

---
+++ nixos/hosts/nixbook/default.nix
@@ -0,0 +1,104 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, pkgs, lib, inputs, outputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      
+      # apple-silicon hardware support
+      inputs.apple-silicon.nixosModules.apple-silicon-support
+      
+      # enable various features
+      ../../features/sound.nix
+      ../../features/bluetooth.nix
+
+      # font config
+      ../../features/hidpi.nix
+    ];
+  
+  home-manager.users.kog = import ../../../home-manager;
+  
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+
+  # Pick only one of the below networking options.
+  networking.networkmanager = {
+    hostName = "nixbook"; # Define your hostname.
+    enable = true;  # Easiest to use and most distros use this by default.
+    wifi.backend = "iwd";
+  }; 
+  # enable GPU support and audio
+  hardware.asahi = {
+    useExperimentalGPUDriver = true;
+    experimentalGPUInstallMode = "replace";
+    setupAsahiSound = true;
+  };
+
+  # backlight control
+  programs.light.enable = true;  
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+    ];
+  };
+ 
+  networking.wireless.iwd = {
+    enable = true;
+    
+    settings.General.EnableNetworkConfiguration = true;
+  };
+  time.timeZone = "Europe/Stockholm";
+
+  services.xserver = {
+  
+    enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+  };
+
+  users.users.kog = {
+     isNormalUser = true;
+     hashedPassword = "$6$WMQGBij0sndI.ZxH$eBSuf/DxQvBnPqGb0qlxXQsUPFkFWc3QKufpTSpfvKMXYcI/NkIZ51vE9vE36388Yj7QnmjACHu3Kbms6dFNm.";
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       firefox
+       tree
+       git
+     ];
+   };
+
+  environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     (vscode-with-extensions.override {
+    vscode = vscodium;
+    vscodeExtensions = with vscode-extensions; [
+      bbenoist.nix
+    ]; 
+  })
+  ];
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
