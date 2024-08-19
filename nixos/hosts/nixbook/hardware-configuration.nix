740253b834c0ea8f26f92bac86f47e23ac6a892c
Alexander Reinthal <email@reinthal.me>
2024-08-19 23:15:22 +0200

    feat: add zram swap

--- nixos/hosts/nixbook/hardware-configuration.nix
+++ nixos/hosts/nixbook/hardware-configuration.nix
@@ -24,8 +24,7 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
-
+zramSwap.enable = true;
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
