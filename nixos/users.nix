538d8d391803661d6cc786fe31f74468482d4842
Alexander Reinthal <email@reinthal.me>
2024-08-22 22:44:52 +0200

    feat: remove stale keys

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,23 +1,22 @@
-{ config, pkgs, ... }:
-
 {
-
+  config,
+  pkgs,
+  ...
+}: {
   security.sudo.enable = true;
-  security.sudo.wheelNeedsPassword = true; 
+  security.sudo.wheelNeedsPassword = true;
   programs.zsh.enable = true;
   users.users.ada = {
     description = "ada";
     isNormalUser = true;
     hashedPassword = "$y$j9T$HWvOqEXKNAo71pncWn0oL.$SCSg00gwPa9UzNjfFQN2q3TTpIt.7LYj7R3li51Q8m8";
-    extraGroups = [ 
-		 "networkmanager" 
-		 "wheel"
+    extraGroups = [
+      "networkmanager"
+      "wheel"
     ];
     shell = pkgs.zsh;
     uid = 1337;
-    openssh.authorizedKeys.keyFiles = [ 
-        ./keys/fireside.pub
-        ./keys/asperitas.pub 
+    openssh.authorizedKeys.keyFiles = [
     ];
   };
 
@@ -26,17 +25,14 @@
     home = "/home/kog";
     isNormalUser = true;
     hashedPassword = "$6$WMQGBij0sndI.ZxH$eBSuf/DxQvBnPqGb0qlxXQsUPFkFWc3QKufpTSpfvKMXYcI/NkIZ51vE9vE36388Yj7QnmjACHu3Kbms6dFNm.";
-    extraGroups = [ 
-		 "networkmanager" 
-		 "wheel"
+    extraGroups = [
+      "networkmanager"
+      "wheel"
     ];
     shell = pkgs.zsh;
     uid = 1000;
-    openssh.authorizedKeys.keyFiles = [ 
-        ./keys/battlestation.pub
-        ./keys/mbpro.pub
-        ./keys/build.pub
+    openssh.authorizedKeys.keyFiles = [
+      ./keys/openpgp.pub
     ];
   };
 }
-
