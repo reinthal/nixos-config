1ba1f0b410fcbae82662f8bef7a4a55a332195c9
Alexander Reinthal <email@reinthal.me>
2024-09-25 00:26:46 +0200

    add more sops

--- home-manager/sops.nix
+++ home-manager/sops.nix
@@ -1,18 +1,22 @@
 {
   inputs,
+  config,
   ...
-}: {
+}: let
+  homeDirectory = config.home.homeDirectory;
+in {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
   sops = {
-    secrets."mail/ekonomigruppen" = {
-#      owner = config.users.users.kog.name;
-      path = "%r/mail/ekonomigruppen";
+    secrets = {
+      "mail/ekonomigruppen" = {
+        path = "${homeDirectory}/.shhh/mail/ekonomigruppen";
+      };
     };
     defaultSopsFile = ../secrets/shhh.yaml;
     age = {
-      keyFile = "/home/kog/.config/sops/age/keys.txt";
+      keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
       generateKey = true;
     };
   };
