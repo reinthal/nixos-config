4e5d85829dde6a0470fd3f41779c52dec3957871
Alexander Reinthal <email@reinthal.me>
2024-09-24 19:44:07 +0200

    add ekonomigruppen email

---
+++ home-manager/email/default.nix
@@ -0,0 +1,40 @@
+{config, ...}: {
+  programs = {
+    alot.enable = true;
+    notmuch.enable = true;
+    mbsync.enable = true;
+  };
+
+  accounts.email.accounts = {
+    "bergaborgen" = {
+      primary = true;
+      mbsync = {
+        enable = true;
+        create = "maildir";
+      };
+      notmuch.enable = true;
+      address = "ekonomigruppen@bergaborgen.se";
+      userName = "ekonomigruppen@bergaborgen.se";
+      realName = "Ekonomigruppen";
+      passwordCommand = [
+        ''cat ${config.sops.secrets."mail/ekonomigruppen".path}''
+      ];
+      imap = {
+        host = "mailcluster.loopia.se";
+        port = 993;
+        tls = {
+          enable = true;
+        };
+      };
+      smtp = {
+        host = "mailcluster.loopia.se";
+        port = 465;
+        tls = {
+          enable = true;
+        };
+      };
+    };
+
+
+  };
+}
