a353e1eaefc2be4c113b0e565acc6ecd09a23b42
Alexander Reinthal <email@reinthal.me>
2024-09-26 21:07:16 +0200

    add environment variables

--- nixos/features/cli/devenvs/datalake-stack.nix
+++ nixos/features/cli/devenvs/datalake-stack.nix
@@ -1,11 +1,7 @@
-{
-  config,
-  ...
-}: let
-  token_path = "${config.sops.secrets."github/datalake-stack/GITHUB_TOKEN".path}";
+{config, ...}: let
+  token_path = "${config.sops.secrets."github/datalakehouse/GITHUB_TOKEN".path}";
 in {
-  # Use sops-secrets to define environment variables
-  environment.variables = {
+  environment.sessionVariables = {
     GITHUB_TOKEN = builtins.readFile token_path;
     GITHUB_USER = "reinthal";
     GITHUB_REPO = "datalake-stack";
