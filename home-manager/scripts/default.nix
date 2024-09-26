5c52ed40c0e2ad72dd70a153ef4141bdf87d518c
Alexander Reinthal <email@reinthal.me>
2024-09-26 22:58:11 +0200

    add toggle-scratchpad script

--- home-manager/scripts/default.nix
+++ home-manager/scripts/default.nix
@@ -6,4 +6,3 @@
     ./toggle-scratchpad.nix
   ];
 }
-
--- home-manager/scripts/toggle-scratchpad.nix
+++ home-manager/scripts/toggle-scratchpad.nix
@@ -1,11 +1,15 @@
 {pkgs, ...}: let
-  toggle-scratchpad = pkgs.writeShellScriptBin "toggle-scratchpad" ''
-    IS_SCRATCHPAD=$(${pkgs.hyprland}/bin/hyprctl activewindow | awk 'NR==6 {print $3}' | grep "special:scratchpad")
-    if echo "$output" | grep -q "special:scratchpad"; then
-    echo "The output contains 'special:scratchpad'"
-else
-    echo "The output does not contain 'special:scratchpad'"
-fi  '';
+  toggle-scratchpad =
+    pkgs.writeShellScriptBin "toggle-scratchpad"
+    ''
+      IS_SCRATCHPAD=$(${pkgs.hyprland}/bin/hyprctl activewindow | awk 'NR==6 {print $3}' | grep "special:scratchpad")
+
+      if [  $IS_SCRATCHPAD ]; then
+         scratchpad -t
+      else
+         ${pkgs.hyprland}/bin/hyprctl dispatch togglespecialworkspace
+      fi
+    '';
 in {
   home.packages = [toggle-scratchpad];
 }
