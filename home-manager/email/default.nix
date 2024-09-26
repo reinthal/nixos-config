7a919dd73064b58fb3ebdf2af576d0b55e847f27
Alexander Reinthal <email@reinthal.me>
2024-09-26 09:02:08 +0200

    fix: linting

--- home-manager/email/default.nix
+++ home-manager/email/default.nix
@@ -1,31 +1,50 @@
-{config, ...}: {
+{config, ...}: let
+  homeDirectory = config.home.homeDirectory;
+in {
   imports = [./neomutt];
   programs = {
     alot.enable = true;
     notmuch.enable = true;
+    astroid = {
+      enable = true;
+      extraConfig = {
+        
+      };
+    };
+    msmtp.enable = true;
     mbsync.enable = true;
   };
 
   accounts.email.accounts = {
     "bergaborgen" = {
       primary = true;
+      address = "ekonomigruppen@bergaborgen.se";
+      userName = "ekonomigruppen@bergaborgen.se";
+      realName = "Ekonomigruppen";
+      passwordCommand = [
+        ''cat ${homeDirectory}/.shhh/mail/ekonomigruppen''
+      ];
+
+      astroid = {
+        enable = true;
+        sendMailCommand = "msmtp --read-envelope-from --read-recipients --account";
+      };
 
       neomutt = {
         enable = true;
         mailboxName = "Ekonomigruppen";
         mailboxType = "imap";
       };
+
       mbsync = {
         enable = true;
         create = "maildir";
       };
+
       notmuch.enable = true;
-      address = "ekonomigruppen@bergaborgen.se";
-      userName = "ekonomigruppen@bergaborgen.se";
-      realName = "Ekonomigruppen";
-      passwordCommand = [
-        ''cat ${config.sops.secrets."mail/ekonomigruppen".path}''
-      ];
+      offlineimap.enable = true;
+      msmtp.enable = true;
+
       imap = {
         host = "mailcluster.loopia.se";
         port = 993;
@@ -33,6 +52,7 @@
           enable = true;
         };
       };
+
       smtp = {
         host = "mailcluster.loopia.se";
         port = 465;
