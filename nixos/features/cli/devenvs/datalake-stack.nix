6d6372fba533384362cfa24c03a3bef719c433de
Alexander Reinthal <email@reinthal.me>
2024-09-26 16:49:50 +0200

    --amend

--- nixos/features/cli/devenvs/datalake-stack.nix
+++ nixos/features/cli/devenvs/datalake-stack.nix
@@ -1,15 +1,9 @@
 {
   config,
-  pkgs,
   ...
 }: let
   token_path = "${config.sops.secrets."github/datalake-stack/GITHUB_TOKEN".path}";
 in {
-  imports = [
-    # Path to the sops-nix folder
-    ./path/to/sops-nix
-  ];
-
   # Use sops-secrets to define environment variables
   environment.variables = {
     GITHUB_TOKEN = builtins.readFile token_path;
