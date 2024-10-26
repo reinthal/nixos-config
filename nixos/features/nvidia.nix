8f385ff901fa3ee1d6dc5fc28aaa3dcd2950f6cd
Alexander Reinthal <email@reinthal.me>
2024-10-26 14:26:13 +0200

    feat: unpin linux kernel from nvidia builds

--- nixos/features/nvidia.nix
+++ nixos/features/nvidia.nix
@@ -1,16 +1,19 @@
-{ config, lib, pkgs, ... }:
 {
-# Enable OpenGL
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # Enable OpenGL
   hardware.graphics = {
     enable = true;
   };
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_10; # TODO: Pinned to 6.10 due to compatability issues
+  #boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_10; # TODO: Pinned to 6.10 due to compatability issues
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
   hardware.nvidia = {
-
     # Modesetting is required.
     modesetting.enable = true;
 
@@ -22,15 +25,15 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
