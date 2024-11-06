def00800438109bef6c91bf5ec8e7e43ddf1b15b
Alexander Reinthal <email@reinthal.me>
2024-11-06 22:51:55 +0100

    fix: removed options in hyprland

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -80,13 +80,7 @@ in {
         rounding = 10;
         inactive_opacity = 0.70;
         active_opacity = 1.00;
-        drop_shadow = "yes";
-        shadow_range = 8;
-        shadow_render_power = 2;
-        "col.shadow" = "rgba(00000044)";
-
         dim_inactive = false;
-
         blur = {
           enabled = true;
           size = 8;
