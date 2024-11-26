704b947affd5fa7c4a9a19711a27fbc9d8193b46
Alexander Reinthal <email@reinthal.me>
2024-11-26 11:27:41 +0100

    feat: pin packages to 24.11, add chromium

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # called derivations that say how to build software.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; #1284004bf6c6e50d8592b6efe83708931e75aec7 nixos-22.11
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -35,6 +35,7 @@ in {
       pass
       tree
       # programming
+      azure-cli
       pyright
       jordanVim
       nixd
