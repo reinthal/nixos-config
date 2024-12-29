902ea4f0ba4c972a77099cfe456109b0a381a3bc
Alexander Reinthal <email@reinthal.me>
2024-12-29 10:15:39 +0100

    feat: add navidrome

--- nixos/features/apps/navidrome.nix
+++ nixos/features/apps/navidrome.nix
@@ -3,7 +3,34 @@
   pkgs,
   ...
 }: {
-  environment.systemPackages = [
-    pkgs.navidrome
-  ];
+  config.systemd.tmpfiles.rules = ["d /home/kog/.navidrome 770 kog users -"];
+  config.virtualisation.oci-containers.containers = {
+    navidrome = {
+      image = "deluan/navidrome:latest";
+
+      extraOptions = [
+        "--hostname"
+        "flix"
+      ];
+
+      ports = [
+        "8095:4533" # Navidrome web interface and API
+      ];
+
+      environment = {
+        PUID = "1000";
+        PGID = "100";
+        ND_SCANINTERVAL = "1h";
+        ND_LOGLEVEL = "info";
+        ND_ENABLESHARING = "true";
+        ND_BASEURL = "https://music.reinthal.me";
+        ND_REVERSEPROXYWHITELIST = "10.22.10.18";
+      };
+
+      volumes = [
+        "/home/kog/.navidrome:/data" # Navidrome configuration and database
+        "/mnt/media/media/music:/music:ro" # Music directory (read-only)
+      ];
+    };
+  };
 }
