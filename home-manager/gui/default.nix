1b7316765bf9ccdb35001015155a2438c902c0c8
Alexander Reinthal <email@reinthal.me>
2025-01-06 13:46:06 +0100

    asdasd

--- home-manager/gui/default.nix
+++ home-manager/gui/default.nix
@@ -27,6 +27,7 @@
       keepassxc
       remmina
       morgen
+      evolution
     ]
     ++ lib.optionals (builtins.currentSystem == "x86_64-linux") [pkgs.slack pkgs.signal-desktop pkgs.spotify];
   xdg = {
