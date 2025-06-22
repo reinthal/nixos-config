effb9e88f141ef14d905f0a564112dd03455f118
Alexander Reinthal <email@reinthal.me>
2025-06-22 20:53:14 +0200

    fix: remove noperm

--- nixos/features/nas.nix
+++ nixos/features/nas.nix
@@ -11,7 +11,7 @@
     options = let
       # this line prevents hanging on network split
       automount_opts = "_netdev,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      permissions = "nobrl,dir_mode=0755,file_mode=0664,uid=1000,gid=100,noperm";
+      permissions = "nobrl,dir_mode=0755,file_mode=0664,uid=1000,gid=100";
     in ["${permissions},${automount_opts},credentials=${config.sops.secrets.nas.path}"];
   };
 }
