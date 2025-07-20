8b7ac6f76d78307b1358de74ec1faa109aa05b83
Alexander Reinthal <email@reinthal.me>
2025-07-20 23:38:55 +0200

    fix: change smb -> nfs agan

--- nixos/features/nas.nix
+++ nixos/features/nas.nix
@@ -3,15 +3,10 @@
   pkgs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [nfs-utils];
-  # Filesystem related settings
+  boot.supportedFilesystems = ["nfs"];
   fileSystems."/mnt/media" = {
     device = "nas.reinthal.me:/mnt/tonberry/media";
     fsType = "nfs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "_netdev,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      nfs_opts = "vers=4,rsize=1048576,wsize=1048576,hard,intr";
-    in ["${nfs_opts},${automount_opts}"];
+    options = ["noauto" "x-systemd.automount" "x-systemd.device-timeout=30"];
   };
 }
--- nixos/users.nix
+++ nixos/users.nix
@@ -10,15 +10,19 @@
   systemd.tmpfiles.rules = [
     "d /mnt/data/kog 0775 kog users - -"
   ];
+  users.groups."reinthal" = {};
+  users.groups.reinthal.gid = 1000;
   users.users.kog = {
     description = "kog";
     home = "/home/kog";
+
     isNormalUser = true;
     hashedPassword = "$6$WMQGBij0sndI.ZxH$eBSuf/DxQvBnPqGb0qlxXQsUPFkFWc3QKufpTSpfvKMXYcI/NkIZ51vE9vE36388Yj7QnmjACHu3Kbms6dFNm.";
     extraGroups = [
       "networkmanager"
       "wheel"
       "wireshark"
+      "reinthal"
     ];
     shell = pkgs.zsh;
     uid = 1000;
