17c130f1bae6508c5d05ca8d172b9e7c472cd909
Alexander Reinthal <email@reinthal.me>
2025-09-25 13:32:22 +0200

    feat: do more configuration of ff

--- home-manager/gui/browser.nix
+++ home-manager/gui/browser.nix
@@ -3,10 +3,39 @@
   inputs,
   ...
 }: {
-  # Module installing brave as default browser
-  home.packages = [
-    firefox
-  ];
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        search.default = "ddg";
+        search.privateDefault = "ddg";
+        containers = {
+            liu = {
+              id = 1;
+              name = "liu";
+              icon =  "briefcase";  
+              color = "blue";
+            };
+
+            umu = {
+              id = 2;
+              name = "umu";
+              icon = "cart";
+              color = "turquoise";
+            };
+
+            aisafety = {
+              name = "ai-safety";
+              id = 3;
+              icon = "fruit";
+              color = "green";
+            };
+          };
+        };
+    };
+  };
 
   home.sessionVariables = {
     DEFAULT_BROWSER = "firefox";
