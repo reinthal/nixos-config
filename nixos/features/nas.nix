c3ba2a1431a607b58d60cab055bced7f3739fb40
Alexander Reinthal <email@reinthal.me>
2025-10-21 20:52:29 +0200

    fix: revert nfs share broken fix for setting default file permissions

--- nixos/features/nas.nix
+++ nixos/features/nas.nix
@@ -7,6 +7,6 @@
   fileSystems."/mnt/media" = {
     device = "nas.reinthal.me:/mnt/tonberry/media";
     fsType = "nfs";
-    options = ["noauto" "x-systemd.automount" "x-systemd.device-timeout=30" "fmask=022" "dmask=022"];
+    options = ["noauto" "x-systemd.automount" "x-systemd.device-timeout=30" "soft"];
   };
 }
