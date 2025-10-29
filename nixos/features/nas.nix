2a7ffaad5296766e4abf256871bbb539fa25befe
Alexander Reinthal <email@reinthal.me>
2025-10-29 08:02:17 +0100

    feat: add anime volume

--- nixos/features/nas.nix
+++ nixos/features/nas.nix
@@ -4,6 +4,12 @@
   ...
 }: {
   boot.supportedFilesystems = ["nfs"];
+  fileSystems."/mnt/anime" = {
+    device = "nas.reinthal.me:/mnt/cactuar/anime";
+    fsType = "nfs";
+    options = ["noauto" "x-systemd.automount" "x-systemd.device-timeout=30" "soft"];
+  };
+
   fileSystems."/mnt/media" = {
     device = "nas.reinthal.me:/mnt/tonberry/media";
     fsType = "nfs";
