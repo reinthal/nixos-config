787b491d32e553c112024ec4a90529d89bd8dda9
Alexander Reinthal <email@reinthal.me>
2025-12-11 15:58:41 +0100

    feat: remove ipv6

--- nixos/features/networking/default.nix
+++ nixos/features/networking/default.nix
@@ -9,11 +9,10 @@ hostname: {
     networkmanager = {
       enable = true; # Easiest to use and most distros use this by default.
       wifi.backend = "iwd";
+      connectionConfig = {
+        "ipv6.method" = "disabled";
+      };
     };
-    # sinkhole mozilla telemetry
-    extraHosts = ''
-      127.0.0.1 incoming.telemetry.mozilla.org
-    '';
     wireless.iwd = {
       enable = true;
       settings.General.EnableNetworkConfiguration = false;
