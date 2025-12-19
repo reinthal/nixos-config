4519f1a130ee3bb720db0eb73f20e2c547f3d4b5
Alexander Reinthal <email@reinthal.me>
2025-12-19 10:14:48 +0100

    feat: open jupyuter ports

--- nixos/hosts/build/default.nix
+++ nixos/hosts/build/default.nix
@@ -83,7 +83,7 @@
 
   programs.dconf.enable = true;
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [9090 8188 8080 1716 9047 10300];
+  networking.firewall.allowedTCPPorts = [9090 8188 8080 1716 9047 10300 2718];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
