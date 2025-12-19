4fd4c45490f48bd8c03510f26cebb0a687e8839d
Alexander Reinthal <email@reinthal.me>
2025-12-19 10:18:41 +0100

    feat: simplify mesage

--- nixos/features/greetd.nix
+++ nixos/features/greetd.nix
@@ -10,7 +10,7 @@
       default_session.command = ''
         ${pkgs.tuigreet}/bin/tuigreet \
           --time \
-          --greeting 'Welcome to NixBook' \
+          --greeting 'Welcome' \
           --theme 'border=darkgray;text=cyan;prompt=green;time=gray;action=cyan;button=green;container=darkgray;input=gray' \
           --asterisks \
           --user-menu \
