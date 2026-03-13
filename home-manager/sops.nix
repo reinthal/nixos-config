c7dbb285afb54b905da60b3e48156b663b11daac
Alexander Reinthal <email@reinthal.me>
2026-03-13 21:37:55 +0100

    fix: remove trash

--- home-manager/sops.nix
+++ home-manager/sops.nix
@@ -10,24 +10,12 @@ in {
   ];
   sops = {
     secrets = {
-      "github/knowyourdata/GITHUB_TOKEN" = {
-        path = "${homeDirectory}/.config/Claude/kyd_githubpat.token";
-        mode = "0400";
-      };
-      "anthropic/api_key" = {
-        path = "${homeDirectory}/.config/Claude/api.key";
-        mode = "0400";
-      };
       "nix_cache/secret_key" = {
         path = "${homeDirectory}/.config/nix/secret.key";
         mode = "0400";
       };
       "nix_cache/nixbuilder" = {
         path = "${homeDirectory}/.aws/credentials";
-        mode = "0400";
-      };
-      "mail/ekonomigruppen" = {
-        path = "${homeDirectory}/.shhh/mail/ekonomigruppen";
         mode = "0400";
       };
       "yt-dlp" = {
