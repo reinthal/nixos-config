c11c70461562e107a6de959917e9edb85c89588c
Alexander Reinthal <email@reinthal.me>
2026-03-21 13:55:48 +0100

    feat: add bash arena address pools

--- nixos/features/apps/docker.nix
+++ nixos/features/apps/docker.nix
@@ -13,6 +13,20 @@
       # Optionally customize rootless Docker daemon settings
       daemon.settings = {
         dns = ["1.1.1.1" "8.8.8.8"];
+        default-address-pools = [
+          {
+            base = "10.0.0.0/8";
+            size = 27;
+          }
+          {
+            base = "172.16.0.0/12";
+            size = 27;
+          }
+          {
+            base = "192.168.0.0/16";
+            size = 27;
+          }
+        ];
         registry-mirrors = ["https://mirror.gcr.io"];
       };
     };
