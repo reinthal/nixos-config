0f3ff3ba51016b7fc9d4df1e5bcae4e1b6bb937a
Alexander Reinthal <email@reinthal.me>
2026-03-28 12:51:43 +0100

    refactor: remove ferroxide

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 {pkgs ? (import ../nixpkgs.nix) {}}: rec {
   custom-fonts = pkgs.callPackage ./fonts {};
-  ferroxide = pkgs.callPackage ./ferroxide.nix {};
   trim-screencast = pkgs.callPackage ./trim-screencast.nix {};
   hyprland-keybindings-menu = pkgs.callPackage ./hyprland-keybindings-menu.nix {};
   wrapWine = pkgs.callPackage ./wrapWine.nix {};
