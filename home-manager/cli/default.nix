b953f753a49d19c71eb3457292ee7c79669e22e7
Alexander Reinthal <email@reinthal.me>
2026-04-08 09:38:00 +0200

    fix(cli): uv ssl path

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -77,6 +77,8 @@ in {
       EDITOR = "nvim";
       KEYID = "1B24ADB218CFB40E";
       SHELL = "${pkgs.zsh}/bin/zsh";
+      # Fix SSL certificate path for uv and other tools expecting OpenSSL default location
+      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
       # ANTHROPIC_API_KEY = builtins.readFile anthropicKey;
     };
   };
